fa2f6d95e71fde24b667cadaa88f5fc4dc08c15c
silva.haberl <silva.haberl@king-ict.hr>
2019-02-26 21:24:07 +0100

    vratit listu poruka na ekran

--- src/index.js
+++ src/index.js
@@ -39,5 +39,27 @@ class App extends React.Component{
             </div>
         )
     }
+}
 
+class MessageList extends React.Component {
+    render() {
+     return{
+        <ul className="message-list">
+            {this.props.messages.map(message => {
+                return (
+                    <li key={message.id}>
+                        <div>
+                            {message.senderId}
+                        </div>
+                        <div>
+                            {message.text}
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+
+
+     }
+    }
 }
