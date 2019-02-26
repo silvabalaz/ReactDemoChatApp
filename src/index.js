0708ad7e7cfee2c39c2370b447554b4f0cb94282
silva.haberl <silva.haberl@king-ict.hr>
2019-02-26 20:32:04 +0100

    vratiti osnovne dijelove UI-a kao komponente

--- src/index.js
+++ src/index.js
@@ -9,4 +9,18 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
+
+class App extends React.Component{
+
+    render() {
+        return (
+            <div className="app">
+                <Title />
+                <MessageList />
+                <SendMessageForm />
+            </div>
+        )
+    }
+
+}
