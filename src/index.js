3b24cf9ff61faa04229dba7e0cd212e19e449853
silva.haberl <silva.haberl@king-ict.hr>
2019-02-26 20:55:01 +0100

    inicijalizirati mockane podatke

--- src/index.js
+++ src/index.js
@@ -5,7 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+const DUMMY_DATA = [
+    {
+        senderId: "silva",
+        text: "Tko još uči React?"
+    },
+    {
+        senderId: "ivana",
+        text: "Ja!"
+    },
+    {
+        senderId: "darko",
+        text: "I ja bi!?"
+    }
+    ]
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
@@ -13,11 +26,15 @@ serviceWorker.unregister()
 
 class App extends React.Component{
 
+    constructor(){
+        super()
+        this.state = { messages : DUMMY_DATA }
+
+    }
     render() {
         return (
             <div className="app">
-                <Title />
-                <MessageList />
+                <MessageList messages = {this.state.messages} />
                 <SendMessageForm />
             </div>
         )
